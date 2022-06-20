73b195b6160fde11c924c2ce16580310e70f6ffb
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-06-20 19:36:59 +0900

    http サーバーを追加

--- main.go
+++ main.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/jaswdr/faker"
 )
 
 func main() {
 	faker := faker.New()
-	name := faker.Person().Name()
-	fmt.Println(name)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		name := faker.Person().Name()
+		fmt.Fprint(w, name)
+	})
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		os.Exit(1)
+	}
 }
